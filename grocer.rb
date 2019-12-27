4a53031b879ac0a82fa4a400fbe871e854efaa49
Douglas Johnson <douglas.j.johnson@gmail.com>
2019-12-27 21:14:40 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -80,22 +80,22 @@ def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
-  coupon = {}
-  coupon_item_name = ""
-  coupon_applied_text = " W/COUPON"
-  coupon_applied_item_name = ""
-  coupon_quantity = 0
-  coupon_cost_per_item = 0.0
+  #coupon = {}
+  #coupon_item_name = ""
+  #coupon_applied_text = " W/COUPON"
+  #coupon_applied_item_name = ""
+  #coupon_quantity = 0
+  #coupon_cost_per_item = 0.0
 
-  item_in_cart = {}
-  item_in_cart_index = 0
-  item_in_cart_quantity = 0
+  #item_in_cart = {}
+  #item_in_cart_index = 0
+  #item_in_cart_quantity = 0
 
-  applied_item_in_cart = {}
-  applied_item_in_cart_index = 0
-  applied_item_in_cart_quantity = 0
+  #applied_item_in_cart = {}
+  #applied_item_in_cart_index = 0
+  #applied_item_in_cart_quantity = 0
 
-  if coupons then
+  #if coupons then
   #  for i in 0...coupons.length
   #    coupon = coupons[i]
   #    coupon_item_name = coupon[:item]
@@ -137,7 +137,7 @@ def apply_coupons(cart, coupons)
   #      end
   #    end
   #  end
-  end
+  #end
 
   pp cart
   #cart = remove_quantity_zero_items (cart)
