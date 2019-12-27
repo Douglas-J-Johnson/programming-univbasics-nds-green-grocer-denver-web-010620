eb1ff9500f2b802b74411d805738c88134f295c1
Douglas Johnson <douglas.j.johnson@gmail.com>
2019-12-27 21:30:25 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -109,8 +109,8 @@ def apply_coupons(cart, coupons)
 
         while item_in_cart_quantity >= coupon_quantity
           item_in_cart_quantity = item_in_cart_quantity - coupon_quantity
-  #        item_in_cart[:count] = item_in_cart_quantity
-  #        cart[item_in_cart_index] = item_in_cart
+          item_in_cart[:count] = item_in_cart_quantity
+          cart[item_in_cart_index] = item_in_cart
 
   #        applied_item_in_cart = find_item_by_name_in_collection(coupon_applied_item_name, cart)
   #        if applied_item_in_cart then
